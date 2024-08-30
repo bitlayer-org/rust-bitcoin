83c8e9488be23e84d3466310bbc0a8846dfb0e9f
Xavier Yin <xavier.yin@bitlayer.ltd>
2024-08-30 12:21:29 +0800

    Increase MAX_VEC_SIZE to 40_000_000

--- bitcoin/src/consensus/encode.rs
+++ bitcoin/src/consensus/encode.rs
@@ -306,7 +306,7 @@ impl<R: Read + ?Sized> ReadExt for R {
 }
 
 /// Maximum size, in bytes, of a vector we are allowed to decode.
-pub const MAX_VEC_SIZE: usize = 4_000_000;
+pub const MAX_VEC_SIZE: usize = 40_000_000;
 
 /// Data which can be encoded in a consensus-consistent way.
 pub trait Encodable {
